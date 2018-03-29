5d3225884ebfa73d3045417e360b27099cbb5433
wangqifu <1030351096@qq.com>
2018-03-29 13:59:09 +0800

    code

---
+++ App/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.ip = new System.Windows.Forms.TextBox();
+            this.userid = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.path = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(7, 110);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "登录";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ip
+            // 
+            this.ip.Location = new System.Drawing.Point(7, 35);
+            this.ip.Name = "ip";
+            this.ip.Size = new System.Drawing.Size(121, 21);
+            this.ip.TabIndex = 1;
+            // 
+            // userid
+            // 
+            this.userid.Location = new System.Drawing.Point(65, 56);
+            this.userid.Name = "userid";
+            this.userid.Size = new System.Drawing.Size(63, 21);
+            this.userid.TabIndex = 2;
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(65, 83);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(63, 21);
+            this.password.TabIndex = 3;
+            this.password.UseSystemPasswordChar = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(2, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "数据库服务器";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "用户名:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "密码:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(12, 136);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "选择数据表";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(7, 164);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.Location = new System.Drawing.Point(14, 257);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "生成实体类";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button3.Location = new System.Drawing.Point(7, 190);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(121, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "选择保存路径:";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // path
+            // 
+            this.path.Location = new System.Drawing.Point(7, 219);
+            this.path.Name = "path";
+            this.path.Size = new System.Drawing.Size(121, 21);
+            this.path.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(143, 307);
+            this.Controls.Add(this.path);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.userid);
+            this.Controls.Add(this.ip);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox ip;
+        private System.Windows.Forms.TextBox userid;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox path;
+    }
+}
+
---
+++ App/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(ip.Text) && !string.IsNullOrWhiteSpace(userid.Text) && !string.IsNullOrWhiteSpace(password.Text))
+            {
+                try
+                {
+                    var list = await Task.Run(() => GetAllDataBase(ip.Text, userid.Text, password.Text));
+                    comboBox1.DataSource = list;
+                    MessageBox.Show("连接成功!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("error,请确认是否为空!");
+            }
+
+        }
+
+        /// <summary>
+        /// 获取指定IP地址的数据库所有数据库实例名。
+        /// </summary>
+        /// <param name="ip">指定的 IP 地址。</param>
+        /// <param name="username">登录数据库的用户名。</param>
+        /// <param name="password">登陆数据库的密码。</param>
+        /// <returns>返回包含数据实例名的列表。</returns>
+        private ArrayList GetAllDataBase(string ip, string username, string password)
+        {
+            ArrayList DBNameList = new ArrayList();
+            SqlConnection Connection = new SqlConnection(
+                String.Format("Data Source={0};DataBase = master;User ID = {1};PWD = {2}", ip, username, password));
+            DataTable DBNameTable = new DataTable();
+            SqlDataAdapter Adapter = new SqlDataAdapter("select name from master..sysdatabases", Connection);
+            lock (Adapter)
+            {
+                Adapter.Fill(DBNameTable);
+            }
+
+            foreach (DataRow row in DBNameTable.Rows)
+            {
+                DBNameList.Add(row["name"]);
+            }
+            Connection.Close();
+            return DBNameList;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog path = new FolderBrowserDialog();
+            path.ShowDialog();
+            this.path.Text = path.SelectedPath;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(comboBox1.SelectedValue.ToString()))
+            {
+                if (!string.IsNullOrWhiteSpace(path.Text))
+                {
+                    SqlConnection Connection = new SqlConnection($"Server={ip.Text};DataBase={comboBox1.SelectedValue.ToString()};Uid={userid.Text};Pwd={password.Text}");
+                    SqlCommand cmd = Connection.CreateCommand();
+                    cmd.CommandText = $"select * from {comboBox1.SelectedValue.ToString()}.information_schema.tables";
+                    Connection.Open();
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    if (dr.HasRows)
+                    {
+                        while (dr.Read())
+                        {
+                            Writeclass(dr["TABLE_NAME"].ToString());
+                        }
+                    }
+                    Connection.Close();
+
+                }
+                else
+                {
+                    MessageBox.Show("未选择保存路径!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("未连接服务器!");
+            }
+
+
+        }
+
+        private void Writeclass(string database)
+        {
+            #region sql语句
+            string sql = $@"SELECT  CASE WHEN col.colorder = 1 THEN obj.name  
+                  ELSE ''  
+             END AS 表名,  
+        col.colorder AS 序号 ,  
+        col.name AS 列名 ,  
+        ISNULL(ep.[value], '') AS 列说明 ,  
+        t.name AS 数据类型 ,  
+        col.length AS 长度 ,  
+        ISNULL(COLUMNPROPERTY(col.id, col.name, 'Scale'), 0) AS 小数位数 ,  
+        CASE WHEN COLUMNPROPERTY(col.id, col.name, 'IsIdentity') = 1 THEN '√'  
+             ELSE ''  
+        END AS 标识 ,  
+        CASE WHEN EXISTS ( SELECT   1  
+                           FROM     dbo.sysindexes si  
+                                    INNER JOIN dbo.sysindexkeys sik ON si.id = sik.id  
+                                                              AND si.indid = sik.indid  
+                                    INNER JOIN dbo.syscolumns sc ON sc.id = sik.id  
+                                                              AND sc.colid = sik.colid  
+                                    INNER JOIN dbo.sysobjects so ON so.name = si.name  
+                                                              AND so.xtype = 'PK'  
+                           WHERE    sc.id = col.id  
+                                    AND sc.colid = col.colid ) THEN '√'  
+             ELSE ''  
+        END AS 主键 ,  
+        CASE WHEN col.isnullable = 1 THEN '√'  
+             ELSE ''  
+        END AS 允许空 ,  
+        ISNULL(comm.text, '') AS 默认值  
+FROM    dbo.syscolumns col  
+        LEFT  JOIN dbo.systypes t ON col.xtype = t.xusertype  
+        inner JOIN dbo.sysobjects obj ON col.id = obj.id  
+                                         AND obj.xtype = 'U'  
+                                         AND obj.status >= 0  
+        LEFT  JOIN dbo.syscomments comm ON col.cdefault = comm.id  
+        LEFT  JOIN sys.extended_properties ep ON col.id = ep.major_id  
+                                                      AND col.colid = ep.minor_id  
+                                                      AND ep.name = 'MS_Description'  
+        LEFT  JOIN sys.extended_properties epTwo ON obj.id = epTwo.major_id  
+                                                         AND epTwo.minor_id = 0  
+                                                         AND epTwo.name = 'MS_Description'  
+WHERE   obj.name = '{database}'
+ORDER BY col.colorder ;  ";
+            #endregion
+            SqlConnection Connection = new SqlConnection($"Server={ip.Text};DataBase={comboBox1.SelectedValue.ToString()};Uid={userid.Text};Pwd={password.Text}");
+            SqlCommand cmd = Connection.CreateCommand();
+            cmd.CommandText = sql;
+            Connection.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            if (dr.HasRows)
+            {
+                while (dr.Read())
+                {
+                    MessageBox.Show(dr[2].ToString());
+                }
+            }
+            Connection.Close();
+        }
+
+
+    }
+}
